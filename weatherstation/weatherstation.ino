24b291acdbca42106660b3af1ac55c672d6a79f0
Sebastian Fedrau <sebastian.fedrau@gmail.com>
2018-02-01 19:05:28 +0100

    updated sketch (event system, transmit stub)

--- weatherstation/weatherstation.ino
+++ weatherstation/weatherstation.ino
@@ -1,42 +1,108 @@
-#include <WeatherLEDs.h>
 #include <WeatherSensors.h>
-
-#define LEDS_DS    7
-#define LEDS_ST_CP 6
-#define LEDS_SH_CP 5
+#include <WeatherLog.h>
+#include <Event.h>
 
 #define DHT11_DIO  12
 #define UV_PIN     A0
 #define REF_3V_PIN A1
 
-WeatherLEDs leds = WeatherLEDs(LEDS_DS, LEDS_SH_CP, LEDS_ST_CP);
+#define MEASURE_INTERVAL  10000ul
+#define TRANSMIT_INTERVAL 30000ul
+
 WeatherSensors sensors(DHT11_DIO, UV_PIN, REF_3V_PIN);
+WeatherLog<512> weatherLog(MEASURE_INTERVAL / 1000);
+
+EventLoop<2> events;
+
+class Measure : public EventCallback
+{
+  public:
+    Measure()
+    {
+      clear(); 
+    }
+ 
+    unsigned long operator()()
+    {
+      Serial.println("MEASURE");
+
+      _success = sensors.measure(_value);
+
+      return 0;
+    }
+
+    void clear()
+    {
+      _success = false;  
+    }
+    
+    inline bool success() { return _success; }
+
+    inline LogValue& value() { return _value; }
+
+  private:
+    LogValue _value;
+    bool _success;
+};
+
+Measure measureEvent;
+
+class Transmit: public EventCallback, public ProcessLogValue
+{
+  public: 
+    unsigned long operator()()
+    {
+      Serial.println("TRANSMIT");
+
+      weatherLog.forEach(*this);
+
+      return TRANSMIT_INTERVAL;
+    }
+
+    void operator()(const LogValue& value)
+    {
+      Serial.print("TIMESTAMP: ");
+      Serial.println(LOG_VALUE_DECODE_TIMESTAMP(value));
+      Serial.print("TEMP: ");
+      Serial.println(LOG_VALUE_DECODE_TEMPERATURE(value));
+      Serial.print("PRESSURE: ");
+      Serial.println(LOG_VALUE_DECODE_PRESSURE(value));
+      Serial.print("HUMIDITY: ");
+      Serial.println(LOG_VALUE_DECODE_HUMIDITY(value));
+      Serial.print("UV: ");
+      Serial.println(LOG_VALUE_DECODE_UV(value));
+    }
+};
+
+Transmit transmitEvent;
 
 void setup()
 {
   Serial.begin(9600);
 
-  leds.begin();
   sensors.begin();
+
+  events.timeout(&measureEvent, 0);
+  events.timeout(&transmitEvent, TRANSMIT_INTERVAL);
 }
 
 void loop()
 {
-  LogValue v;
+  events.iteration();
 
-  if(sensors.measure(v))
+  if(measureEvent.completed())
   {
-    Serial.print("TIMESTAMP: ");
-    Serial.println(LOG_VALUE_DECODE_TIMESTAMP(v));
-    Serial.print("TEMP: ");
-    Serial.println(LOG_VALUE_DECODE_TEMPERATURE(v));
-    Serial.print("PRESSURE: ");
-    Serial.println(LOG_VALUE_DECODE_PRESSURE(v));
-    Serial.print("HUMIDITY: ");
-    Serial.println(LOG_VALUE_DECODE_HUMIDITY(v));
-    Serial.print("UV: ");
-    Serial.println(LOG_VALUE_DECODE_UV(v));
+    if(measureEvent.success())
+    {
+      LogValue value = measureEvent.value();
+
+      weatherLog.append(value);
+    }
+
+    measureEvent.clear();
+
+    events.timeout(&measureEvent, MEASURE_INTERVAL);
   }
   
-  delay(5000);
+  delay(500);
 }
